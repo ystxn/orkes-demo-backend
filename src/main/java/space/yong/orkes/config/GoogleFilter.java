23f161097d6e3c78f07dc608bcae65b26b414896
Yong Sheng Tan <yong@orkes.io>
2024-06-14 17:01:38 +0800

    Added debug for GoogleFilter

--- src/main/java/space/yong/orkes/config/GoogleFilter.java
+++ src/main/java/space/yong/orkes/config/GoogleFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.List;
 
+@Slf4j
 @Service
 public class GoogleFilter extends OncePerRequestFilter {
     @Value("${google.client-id}")
@@ -28,21 +30,32 @@ public class GoogleFilter extends OncePerRequestFilter {
         @NonNull HttpServletResponse response,
         @NonNull FilterChain chain
     ) throws ServletException, IOException {
+        log.debug("Entered GoogleFilter with clientId: {}", clientId);
         final String authHeader = request.getHeader("Authorization");
+
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            log.debug("Bearer exists");
             try {
                 String token = authHeader.substring(7);
                 var verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
                     .setAudience(List.of(clientId))
                     .build();
+                log.debug("Verifier built");
                 GoogleIdToken idToken = GoogleIdToken.parse(verifier.getJsonFactory(), token);
+                log.debug("Token parsed");
                 verifier.verify(idToken);
+                log.debug("Token verified");
                 GoogleIdToken.Payload payload = idToken.getPayload();
+                log.debug("Payload extracted");
                 var authToken = new UsernamePasswordAuthenticationToken(payload.getEmail(), null, null);
+                log.debug("Auth token created");
                 SecurityContextHolder.getContext().setAuthentication(authToken);
+                log.debug("Security context set");
             } catch (Exception e) {
+                log.error("Exception thrown", e);
                 response.setStatus(401);
             }
+            log.debug("End of GoogleFilter");
         }
         chain.doFilter(request, response);
     }
