30e881b7d80a237980c02ac93dd52bcd8fe990e2
Yong Sheng Tan <yong@orkes.io>
2024-06-18 17:45:24 +0800

    Add human tasks query

--- src/main/java/space/yong/orkes/services/OrkesService.java
+++ src/main/java/space/yong/orkes/services/OrkesService.java
@@ -3,21 +3,27 @@ package space.yong.orkes.services;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.conductor.sdk.workflow.executor.WorkflowExecutor;
+import io.orkes.conductor.client.ApiClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class OrkesService {
-    private final WorkflowExecutor executor;
     private final ObjectMapper objectMapper;
+    private final WorkflowExecutor executor;
+    private final ApiClient apiClient;
 
     @PostMapping("execute/{workflowName}/{version}")
     public Map<String, Object> execute(
@@ -31,5 +37,31 @@ public class OrkesService {
         Map<String, Object> output = executor.executeWorkflow(workflowName, version, input).join().getOutput();
         log.info("Result: {}", objectMapper.writeValueAsString(output));
         return output;
+    }
+
+    @GetMapping("human-tasks")
+    public Object listHumanTasks(Authentication auth) {
+        log.info("Listing human tasks for {}", auth.getPrincipal());
+        var call = apiClient.buildCall(
+            "/human/tasks/search",
+            "POST",
+            new ArrayList<>(),
+            new ArrayList<>(),
+            Map.of(
+                "size", 15,
+                "states", List.of("ASSIGNED"),
+                "assignees", List.of(
+                    Map.of(
+                        "userType", "CONDUCTOR_USER",
+                        "user", auth.getPrincipal()
+                    )
+                )
+            ),
+            new HashMap<>(),
+            Map.of(),
+            new String[] {"api_key"},
+            null);
+        var response = apiClient.execute(call, Map.class);
+        return response.getData();
     }
 }
