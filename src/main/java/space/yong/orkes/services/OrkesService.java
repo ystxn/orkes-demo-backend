d7505a47f4585f1283852551d4f866530cc21d52
Yong Sheng Tan <yong@orkes.io>
2025-01-17 16:56:09 +0800

    Added correlation id to execute call and added terminate call

--- src/main/java/space/yong/orkes/services/OrkesService.java
+++ src/main/java/space/yong/orkes/services/OrkesService.java
@@ -1,5 +1,6 @@
 package space.yong.orkes.services;
 
+import static com.netflix.conductor.client.http.ConductorClientRequest.Method.DELETE;
 import static com.netflix.conductor.client.http.ConductorClientRequest.Method.GET;
 import static com.netflix.conductor.client.http.ConductorClientRequest.Method.POST;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,16 +12,17 @@ import com.netflix.conductor.client.http.ConductorClientResponse;
 import com.netflix.conductor.client.http.WorkflowClient;
 import com.netflix.conductor.common.metadata.workflow.StartWorkflowRequest;
 import com.netflix.conductor.common.run.Workflow;
-import com.netflix.conductor.sdk.workflow.executor.WorkflowExecutor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,14 +31,13 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class OrkesService {
     private final ObjectMapper objectMapper;
-    private final WorkflowExecutor executor;
     private final ConductorClient client;
     private final WorkflowClient workflowClient;
 
     public record HumanTaskUserAssignee(String userType, Object user) {}
 
     @PostMapping("execute/{workflowName}/{version}")
-    public Map<String, Object> executeWorkflow(
+    public Object executeWorkflow(
         Authentication auth,
         @PathVariable String workflowName,
         @PathVariable int version,
@@ -44,9 +45,23 @@ public class OrkesService {
     ) throws JsonProcessingException {
         String inputString = objectMapper.writeValueAsString(input);
         log.info("{} executing {} (v{}): {}", auth.getPrincipal(), workflowName, version, inputString);
-        Map<String, Object> output = executor.executeWorkflow(workflowName, version, input).join().getOutput();
-        log.info("Result: {}", objectMapper.writeValueAsString(output));
-        return output;
+
+        Map <String, Object> body = new HashMap<>();
+        if (input.containsKey("correlationId")) {
+            body.put("correlationId", input.get("correlationId").toString());
+            input.remove("correlationId");
+        }
+        body.put("input", input);
+
+        var request = ConductorClientRequest.builder()
+            .method(POST)
+            .path("/workflow/execute/{name}/{version}")
+            .addPathParam("name", workflowName)
+            .addPathParam("version", version)
+            .body(body)
+            .build();
+        ConductorClientResponse<Object> resp = client.execute(request, new TypeReference<>() {});
+        return resp.getData();
     }
 
     @PostMapping("start/{workflowName}/{version}")
@@ -203,5 +218,20 @@ public class OrkesService {
             .build();
         ConductorClientResponse<Object> resp = client.execute(request, new TypeReference<>() {});
         return resp.getData();
+    }
+
+    @DeleteMapping("terminate/{workflowId}")
+    public void terminateWorkflow(
+        Authentication auth,
+        @PathVariable String workflowId,
+        @RequestParam(required = false) String reason
+    ) {
+        log.info("{} terminating workflow {}", auth.getPrincipal(), workflowId);
+        client.execute(ConductorClientRequest.builder()
+            .method(DELETE)
+            .path("/workflow/{workflowId}")
+            .addPathParam("workflowId", workflowId)
+            .addQueryParam("reason", reason)
+            .build());
     }
 }
